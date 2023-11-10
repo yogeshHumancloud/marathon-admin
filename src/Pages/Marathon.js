2f7d473f9243bb1c8ec5ffcded93fb99b6d2bced
yogeshHumancloud <yogesh.vitekar@humancloud.co.in>
2023-11-10 15:18:04 +0530

    added form

--- src/Pages/Marathon.js
+++ src/Pages/Marathon.js
@@ -1,15 +1,21 @@
-import { Box, Button, Card, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, Dialog, Stack, Typography } from "@mui/material";
 import React, { useState } from "react";
 import CustomeTable from "../Componetns/CustomeTable";
 import { useNavigate } from "react-router";
 import { dummydata } from "../Componetns/dummydata";
 import DeleteConfirmation from "../Componetns/DeleteConfirmation";
+import { FileUploader } from "react-drag-drop-files";
 
 const Marathon = () => {
   const navigate = useNavigate();
+  const [file, setFile] = useState(null);
   const [deletePopup, setDeletePopup] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [openPopUp, setOpenPopUp] = useState(false);
   const header = ["name", "email", "mobile", "age", "edit", "delete"];
+  const handleFileChange = (data) => {
+    setFile(data);
+  };
 
   const onEdit = (row) => {
     navigate(`/edit/${row.id}`);
@@ -55,10 +61,44 @@ const Marathon = () => {
           }}
         >
           <Typography variant="h4">Marathon's</Typography>
-          <Button onClick={() => navigate("/add")} variant="contained">
+          <Button onClick={() => setOpenPopUp(true)} variant="contained">
             Add
           </Button>
         </Stack>
+        <Dialog
+          PaperProps={{ sx: { height: "45vh", width: "50vw" } }}
+          open={openPopUp}
+          onClose={() => setOpenPopUp(false)}
+        >
+          <Stack
+            marginTop={"2rem"}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            spacing={4}
+          >
+            <Typography variant="h5">
+              {" "}
+              Please Upload Your Excel File here
+            </Typography>
+            <FileUploader
+              handleChange={(file) => handleFileChange(file)}
+              name="avatar"
+              types={["xlsx", "xls", "csv"]}
+            />
+            <Typography>
+              or Fill{" "}
+              <Button sx={{ p: 0 }} onClick={() => navigate("/add")}>
+                this
+              </Button>{" "}
+              form{" "}
+            </Typography>
+            <Box sx={{ display: "flex"}}>
+              <Button onClick={() => setOpenPopUp(false)}>cancel</Button>
+              <Button disabled={!file}>Save</Button>
+            </Box>
+          </Stack>
+        </Dialog>
 
         <CustomeTable
           header={header}
